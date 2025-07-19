40504b5e4ad4dd143c66e2273bc83575a49d94e0
Gabriel Ravanhan <gabriel.ravanhan@outlook.com>
2025-07-18 21:15:23 -0300

    Exercício 11

--- src/main/java/Exercicio11.java
+++ src/main/java/Exercicio11.java
@@ -1,2 +1,18 @@
-public class Exercicio09 {
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Exercicio11 {
+
+    /**
+     * <h1>Encontrar o Nome Mais Longo</h1>
+     * <p>Dada uma lista de nomes (List(String)), encontre o nome com o maior número de caracteres. Em caso de empate, retorne o primeiro encontrado. Use max(Comparator).</p>
+     */
+    public static void main(String[] args) {
+        List<String> nomes = Arrays.asList("Cleiton", "Carolina", "Waldemar");
+
+        String nomeComMaiorNumeroDeCaracteres = nomes.stream().max(Comparator.comparingInt(String::length)).stream().findFirst().orElse("Lista vazia.");
+
+        System.out.println(nomeComMaiorNumeroDeCaracteres);
+    }
 }
