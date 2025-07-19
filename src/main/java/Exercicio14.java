cc1526705031f5a184ac63841644866a4b83b67c
Gabriel Ravanhan <gabriel.ravanhan@outlook.com>
2025-07-18 21:36:10 -0300

    Exercício 14

--- src/main/java/Exercicio14.java
+++ src/main/java/Exercicio14.java
@@ -1,2 +1,19 @@
-public class Exercicio09 {
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Exercicio14 {
+
+    /**
+     * <h1>Remover Palavras Duplicadas e Ordenar por Tamanho</h1>
+     * <p>Dada uma lista de palavras, remova as duplicadas e ordene as restantes pelo tamanho da palavra (do menor para o maior). Use distinct e Comparator.comparingInt(String::length).</p>
+     */
+    public static void main(String[] args) {
+        List<String> palavras = Arrays.asList("Reinaldinho", "Rei", "Reinaldo");
+
+        palavras = palavras.stream().distinct().sorted(Comparator.comparingInt(String::length)).collect(Collectors.toList());
+
+        palavras.forEach(System.out::println);
+    }
 }
