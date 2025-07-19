855f72d290237169db433ea3044248dd27806e69
Gabriel Ravanhan <gabriel.ravanhan@outlook.com>
2025-07-18 21:02:37 -0300

    Exercício 10

--- src/main/java/Exercicio10.java
+++ src/main/java/Exercicio10.java
@@ -1,2 +1,17 @@
-public class Exercicio09 {
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercicio10 {
+
+    /**
+     * <h1>Verificar se Todos os Números São Positivos</h1>
+     * <p>Dada uma lista de inteiros, use allMatch para verificar se todos os números são positivos.</p>
+     */
+    public static void main(String[] args) {
+        List<Integer> inteiros = Arrays.asList(0, 1, 2, 5, 4, 9, -8);
+
+        boolean todosSaoPositivos = inteiros.stream().allMatch(numero -> numero > 0);
+
+        System.out.println("Todos são positivos: " + (todosSaoPositivos ? "si." : "não"));
+    }
 }
